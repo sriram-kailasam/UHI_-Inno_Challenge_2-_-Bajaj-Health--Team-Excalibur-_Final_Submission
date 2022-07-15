8ca4b7b690b79ee6ebc72760a17d0c5ec2166000
Ashutosh Agrawal <ashutoshagrawal@L-C02DC0GGP3YV.local>
2022-07-15 11:11:14 +0530

    Added tailwindcss and removed trpc from hspa client

--- hspa-client/src/App.tsx
+++ hspa-client/src/App.tsx
@@ -1,24 +1,13 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { Hello } from './hello';
-import { trpc } from './utils/trpc';
 
 function App() {
   const [queryClient] = useState(() => new QueryClient());
-  const [trpcClient] = useState(() =>
-    trpc.createClient({
-      url: '/api/trpc',
-    }),
-  );
 
- 
     return (
-      <trpc.Provider client={trpcClient} queryClient={queryClient}>
         <QueryClientProvider client={queryClient}>
           <p>Hello</p>
-          <Hello></Hello>
         </QueryClientProvider>
-      </trpc.Provider>
     );
   
 }
--- hspa-client/src/hello.tsx
+++
@@ -1,9 +0,0 @@
-import { trpc } from "./utils/trpc"
-
-export const Hello = () => {
-  const hello = trpc.useQuery(['hello'])
-  if (hello.isLoading) return <p>Loading...</p>
-  return (
-    <div>{hello.data?.greeting}</div>
-  )
-}
