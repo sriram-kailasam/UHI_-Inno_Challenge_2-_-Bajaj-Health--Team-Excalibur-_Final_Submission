e1bc0908d3e40b7f00eb8fbbb480d220fcbe42bf
Ashutosh Agrawal <ashutoshagrawal@L-C02DC0GGP3YV.local>
2022-07-16 20:45:52 +0530

    Added basic doctor search page and basic navigation on hspa

--- hspa-client/src/app/routes/AppRoutes.tsx
+++ hspa-client/src/app/routes/AppRoutes.tsx
@@ -3,6 +3,7 @@ import Login from "modules/auth/components/login/Login";
 import Dashboard from "modules/dashboard/Dashboard";
 import PatientDetails from 'modules/patient-details/PatientDetails';
 import RouteHspaOutlet from "./RouteHspaOutlet";
+import DoctorSearch from "modules/doctor-search/DoctorSearch";
 
 const AppRoutes = () => {
   return (
@@ -11,6 +12,7 @@ const AppRoutes = () => {
         <Route path="login" element={<Login />} />
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="patientDetails" element={<PatientDetails />} />
+        <Route path="doctorSearch" element={<DoctorSearch />} />
       </Route>
     </Routes>
   )
