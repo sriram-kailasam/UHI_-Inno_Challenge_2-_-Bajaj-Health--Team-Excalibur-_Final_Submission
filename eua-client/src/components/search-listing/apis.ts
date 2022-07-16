e9eaaee18c2f67963f78e1a0973f1c7147976abe
Shyam Mittal <mittalshyam1007@gmail.com>
2022-07-16 22:38:46 +0530

    update react query

--- eua-client/src/components/search-listing/apis.ts
+++ eua-client/src/components/search-listing/apis.ts
@@ -1,6 +1,10 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { baseUrl } from "../../utils/constants";
+import { IDoctor } from "./doctor-mock";
 
-export const searchDoctor = (keyword: string) => {
+export const searchDoctor: (k: string) => Promise<AxiosResponse<IDoctor[]>> = (
+    keyword: string
+) => {
+    console.log("kjnkj", keyword);
     return axios.get(`${baseUrl}/eua/searchDoctors?name=${keyword}`);
 };
--- eua-client/src/components/search-listing/index.tsx
+++ eua-client/src/components/search-listing/index.tsx
@@ -1,10 +1,12 @@
 import { SearchOutlined } from "@ant-design/icons";
 import { Input } from "antd";
+import { AxiosResponse } from "axios";
 import React, { useState } from "react";
 import { useQuery } from "react-query";
 import { useDebounce } from "../../utils/helper";
 import { searchDoctor } from "./apis";
 import DoctorCard from "./doctor-card";
+import { IDoctor } from "./doctor-mock";
 import "./styles.scss";
 
 const SearchListing = () => {
@@ -14,7 +16,12 @@ const SearchListing = () => {
         setSearchText(e.target.value);
     };
 
-    // const {} = useQuery(["doctor-search", debSearchText], searchDoctor);
+    const { data: someData } = useQuery<AxiosResponse<IDoctor[]>, Error>(
+        ["doctor-search", debSearchText],
+        () => searchDoctor(debSearchText)
+    );
+
+    console.log("someData", someData);
 
     return (
         <div className="search-listing">
