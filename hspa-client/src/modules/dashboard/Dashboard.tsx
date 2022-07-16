415a7cad3251aa3e16afb519718803520cebc06c
Ashutosh Agrawal <ashutoshagrawal@L-C02DC0GGP3YV.local>
2022-07-16 16:25:33 +0530

    UI fixes for hspa dashboard

--- hspa-client/src/modules/dashboard/Dashboard.tsx
+++ hspa-client/src/modules/dashboard/Dashboard.tsx
@@ -19,7 +19,7 @@ const Dashboard = () => {
   return (
     <div className='max-w-md flex flex-col items-center m-auto'>
       <Header heading='Patient Appointment List' />
-      <div className='bg-[#f2f3f9] h-screen w-full p-4 mt-16'>
+      <div className='bg-[#f2f3f9] h-full w-full p-4 mt-16'>
         {appointmentList.map((appt) => 
           <AppointmentCard {...appt} />
         )}
--- hspa-client/src/modules/dashboard/components/Header.tsx
+++ hspa-client/src/modules/dashboard/components/Header.tsx
@@ -3,7 +3,7 @@ import { HeaderProps } from '../types';
 
 const Header: React.FC<HeaderProps> = ({ heading }) => {
   return (
-    <div className="h-16 bg-white fixed w-full top-0 flex items-center max-w-md">
+    <div className="h-16 bg-white fixed w-full top-0 flex items-center max-w-md z-[1]">
       <img src={LeftArrow} alt="Left Arrow" className='ml-2'/>
       <p className='text-base text-black font-semibold w-full text-center'>{heading}</p>
     </div>
