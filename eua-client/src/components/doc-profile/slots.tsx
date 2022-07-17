7dea50cc69cc377be8cbe97114d9b3455b1e2f1f
Shyam Mittal <mittalshyam1007@gmail.com>
2022-07-17 13:54:26 +0530

    add loaders

--- eua-client/src/components/doc-profile/slots.tsx
+++ eua-client/src/components/doc-profile/slots.tsx
@@ -1,33 +1,44 @@
 import { ISlot } from "../search-listing/doctor-mock";
 import dayjs from "dayjs";
+import Loading from "../../elements/loading";
 
 const Slots = ({
     slots,
     onSlotSelect,
     selectedSlotId,
+    isLoading,
 }: {
     slots: ISlot[];
     onSlotSelect: (slotId: string) => void;
     selectedSlotId: string;
-}) => (
-    <div className="slots-container">
-        {slots?.map((eachSlot) => {
-            return (
-                <div
-                    className={`each-slot${
-                        selectedSlotId === eachSlot.slotId ? " selected" : ""
-                    }`}
-                    onClick={() => {
-                        onSlotSelect(eachSlot.slotId);
-                    }}
-                >
-                    <span className="slot-label">
-                        {dayjs(eachSlot.startTime).format("hh:mm A, DD MMM")}
-                    </span>
-                </div>
-            );
-        })}
-    </div>
-);
+    isLoading: boolean;
+}) => {
+    return (
+        <div className="slots-container">
+            {isLoading && <Loading />}
+            {!isLoading && !slots?.length && <span>No Slots Found</span>}
+            {slots?.map((eachSlot) => {
+                return (
+                    <div
+                        className={`each-slot${
+                            selectedSlotId === eachSlot.slotId
+                                ? " selected"
+                                : ""
+                        }`}
+                        onClick={() => {
+                            onSlotSelect(eachSlot.slotId);
+                        }}
+                    >
+                        <span className="slot-label">
+                            {dayjs(eachSlot.startTime).format(
+                                "hh:mm A, DD MMM"
+                            )}
+                        </span>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
 
 export default Slots;
